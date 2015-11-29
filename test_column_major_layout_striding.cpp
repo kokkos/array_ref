4b2ed4fd5968d0bff8c6e59c3a26fa689aa5b7eb
Bryce Adelstein Lelbach aka wash <balelbach@lbl.gov>
2015-11-28 19:59:51 -0800

    Add 2D test cases and bounds-checking to the column_major_layout striding tests.

--- test_column_major_layout_striding.cpp
+++ test_column_major_layout_striding.cpp
@@ -39,20 +39,22 @@ void test_1d_static()
     {
         BOOST_TEST_EQ((l.index(d, i)), i);
 
-        BOOST_TEST_EQ(&(l.access(dptr, l.index(d, i))), &(dptr[i])); 
+        BOOST_TEST_EQ(&(l.access(dptr, l.index(d, i))), &(dptr[i]));
 
         l.access(dptr, l.index(d, i)) = 42;
 
-        BOOST_TEST_EQ((l.access(dptr, l.index(d, i))), 42); 
+        BOOST_TEST_EQ((l.access(dptr, l.index(d, i))), 42);
     }
 
     // Set every Nth element to 17. 
     for (auto i = 0; i < get_value<0>(sub_d); ++i)
     {
-        BOOST_TEST_EQ((sub_l.index(sub_d, i)), get_value<0>(sub_s)*i);
+        auto const true_idx = i*get_value<0>(sub_s);
+
+        BOOST_TEST_EQ((sub_l.index(sub_d, i)), true_idx);
 
         BOOST_TEST_EQ(&(sub_l.access(dptr, sub_l.index(sub_d, i)))
-                    , &(dptr[get_value<0>(sub_s)*i])); 
+                    , &(dptr[true_idx])); 
 
         sub_l.access(dptr, sub_l.index(sub_d, i)) = 17;
 
@@ -93,28 +95,172 @@ void test_1d_dynamic()
     // Set every Nth element to 17. 
     for (auto i = 0; i < get_value<0>(sub_d); ++i)
     {
-        BOOST_TEST_EQ((sub_l.index(sub_d, i)), get_value<0>(sub_s)*i);
+        auto const true_idx = i*get_value<0>(sub_s);
+
+        BOOST_TEST_EQ((sub_l.index(sub_d, i)), true_idx);
 
         BOOST_TEST_EQ(&(sub_l.access(dptr, sub_l.index(sub_d, i)))
-                    , &(dptr[get_value<0>(sub_s)*i])); 
+                    , &(dptr[true_idx])); 
 
         sub_l.access(dptr, sub_l.index(sub_d, i)) = 17;
 
         BOOST_TEST_EQ((sub_l.access(dptr, sub_l.index(sub_d, i))), 17);
+
+        // Bounds-checking.
+        BOOST_TEST_EQ((data.at(sub_l.index(sub_d, i))), 17);
     }
 
-    // Check final structure. 
+    // Check final structure.
     for (auto i = 0; i < get_value<0>(d); ++i)
     {
         if (0 == (i % get_value<0>(sub_s)))
+        {
             BOOST_TEST_EQ((l.access(dptr, l.index(d, i))), 17);
+
+            // Bounds-checking.
+            BOOST_TEST_EQ((data.at(l.index(d, i))), 17);
+        }
         else
+        {
             BOOST_TEST_EQ((l.access(dptr, l.index(d, i))), 42);
+
+            // Bounds-checking.
+            BOOST_TEST_EQ((data.at(l.index(d, i))), 42);
+        }
+    }
+} // }}}
+
+template <std::size_t N, std::size_t M, std::size_t X, std::size_t Y>
+void test_2d_static()
+{ // {{{
+    static_assert(0 == (X % N), "X must be divisable by N");
+    static_assert(0 == (Y % M), "Y must be divisable by M");
+
+    dims<X, Y> d;
+    dims<1, 1>  s;
+
+    dims<X/N, Y/M> sub_d; 
+    dims<N,   M  > sub_s;
+
+    column_major_layout<int, decltype(s), dims<0, 0> > l;
+
+    column_major_layout<int, decltype(sub_s), dims<0, 0> > sub_l;
+
+    int dptr[X*Y];
+
+    // Set all elements to 42.
+    for (auto j = 0; j < get_value<1>(d); ++j)
+    for (auto i = 0; i < get_value<0>(d); ++i)
+    {
+        auto const true_idx = i + j*get_value<0>(d);
+
+        BOOST_TEST_EQ((l.index(d, i, j)), true_idx);
+
+        BOOST_TEST_EQ(&(l.access(dptr, l.index(d, i, j))), &(dptr[true_idx]));
+
+        l.access(dptr, l.index(d, i, j)) = 42;
+
+        BOOST_TEST_EQ((l.access(dptr, l.index(d, i, j))), 42);
+    }
+
+    // Set every (Nth, Mth) element to 17.
+    for (auto j = 0; j < get_value<1>(sub_d); ++j)
+    for (auto i = 0; i < get_value<0>(sub_d); ++i)
+    {
+        auto const true_idx = (i + j*get_value<1>(sub_s)*get_value<0>(sub_d))
+                            * get_value<0>(sub_s);
+
+        BOOST_TEST_EQ((sub_l.index(sub_d, i, j)), true_idx);
+
+        BOOST_TEST_EQ(&(sub_l.access(dptr, sub_l.index(sub_d, i, j)))
+                    , &(dptr[true_idx])); 
+
+        sub_l.access(dptr, sub_l.index(sub_d, i, j)) = 17;
+
+        BOOST_TEST_EQ((sub_l.access(dptr, sub_l.index(sub_d, i, j))), 17);
+    }
+
+    // Check final structure. 
+    for (auto j = 0; j < get_value<1>(d); ++j)
+    for (auto i = 0; i < get_value<0>(d); ++i)
+    {
+        if (  (0 == (i % get_value<0>(sub_s)))
+           && (0 == (j % get_value<1>(sub_s)))
+           )
+            BOOST_TEST_EQ((l.access(dptr, l.index(d, i, j))), 17);
+        else
+            BOOST_TEST_EQ((l.access(dptr, l.index(d, i, j))), 42);
+    }
+} // }}}
+
+template <std::size_t N, std::size_t M, std::size_t X, std::size_t Y>
+void test_2d_dynamic()
+{ // {{{
+    static_assert(0 == (X % N), "X must be divisable by N");
+    static_assert(0 == (Y % M), "Y must be divisable by M");
+
+    dims<dyn, dyn> d(X, Y);
+    dims<dyn, dyn> s(1, 1);
+
+    dims<dyn, dyn> sub_d(X/N, Y/M); 
+    dims<dyn, dyn> sub_s(N,   M  );;
+
+    column_major_layout<int, decltype(s), dims<0, 0> >
+        l(s, dims<0, 0>());
+
+    column_major_layout<int, decltype(sub_s), dims<0, 0> >
+        sub_l(sub_s, dims<0, 0>());
+
+    // Initialize all elements as 42.
+    std::vector<int> data(get_value<0>(d)*get_value<1>(d), 42);
+    int* dptr = data.data();
+
+    // Set every (Nth, Mth) element to 17.
+    for (auto j = 0; j < get_value<1>(sub_d); ++j)
+    for (auto i = 0; i < get_value<0>(sub_d); ++i)
+    {
+        auto const true_idx = (i + j*get_value<1>(sub_s)*get_value<0>(sub_d))
+                            * get_value<0>(sub_s);
+
+        BOOST_TEST_EQ((sub_l.index(sub_d, i, j)), true_idx);
+
+        BOOST_TEST_EQ(&(sub_l.access(dptr, sub_l.index(sub_d, i, j)))
+                    , &(dptr[true_idx])); 
+
+        sub_l.access(dptr, sub_l.index(sub_d, i, j)) = 17;
+
+        BOOST_TEST_EQ((sub_l.access(dptr, sub_l.index(sub_d, i, j))), 17);
+
+        // Bounds-checking.
+        BOOST_TEST_EQ((data.at(sub_l.index(sub_d, i, j))), 17);
+    }
+
+    // Check final structure. 
+    for (auto j = 0; j < get_value<1>(d); ++j)
+    for (auto i = 0; i < get_value<0>(d); ++i)
+    {
+        if (  (0 == (i % get_value<0>(sub_s)))
+           && (0 == (j % get_value<1>(sub_s)))
+           )
+        {
+            BOOST_TEST_EQ((l.access(dptr, l.index(d, i, j))), 17);
+
+            // Bounds-checking.
+            BOOST_TEST_EQ((data.at(l.index(d, i, j))), 17);
+        }
+        else
+        {
+            BOOST_TEST_EQ((l.access(dptr, l.index(d, i, j))), 42);
+
+            // Bounds-checking.
+            BOOST_TEST_EQ((data.at(l.index(d, i, j))), 42);
+        }
     }
 } // }}}
 
 int main()
 {
+    // 1D Static
     test_1d_static<1, 1>();
     test_1d_static<1, 30>();
 
@@ -123,6 +269,7 @@ int main()
     test_1d_static<10, 30>();
     test_1d_static<30, 30>();
 
+    // 1D Dynamic
     test_1d_dynamic<1, 1>();
     test_1d_dynamic<1, 30>();
 
@@ -130,6 +277,36 @@ int main()
     test_1d_dynamic<5,  30>();
     test_1d_dynamic<10, 30>();
     test_1d_dynamic<30, 30>();
+
+    // 2D Static
+    test_2d_static<1, 1, 1,  1 >();
+
+    test_2d_static<1, 1, 30, 1 >();
+    test_2d_static<1, 1, 1,  30>();
+    test_2d_static<1, 1, 30, 30>();
+
+    test_2d_static<2, 1, 30, 1 >();
+    test_2d_static<1, 2, 1,  30>();
+    test_2d_static<2, 2, 30, 30>();
+
+    test_2d_static<5, 1, 30, 1 >();
+    test_2d_static<1, 5, 1,  30>();
+    test_2d_static<5, 5, 30, 30>();
+
+    // 2D Dynamic
+    test_2d_dynamic<1, 1, 1,  1 >();
+
+    test_2d_dynamic<1, 1, 30, 1 >();
+    test_2d_dynamic<1, 1, 1,  30>();
+    test_2d_dynamic<1, 1, 30, 30>();
+
+    test_2d_dynamic<2, 1, 30, 1 >();
+    test_2d_dynamic<1, 2, 1,  30>();
+    test_2d_dynamic<2, 2, 30, 30>();
+
+    test_2d_dynamic<5, 1, 30, 1 >();
+    test_2d_dynamic<1, 5, 1,  30>();
+    test_2d_dynamic<5, 5, 30, 30>();
 
     return boost::report_errors();
 }
