7d846684754ab19e151658326cb9b969eb18daf3
Carter Edwards <hcedwar@sandia.gov>
2016-05-19 17:37:51 -0600

    Filling out subarray demonstration

--- reference_K/test/test_array.cpp
+++ reference_K/test/test_array.cpp
@@ -102,13 +102,31 @@ void test_array_subarray()
 {
   using array_t = std::experimental::array_ref<int[][20][30][40]> ;
   using range_t = std::pair<size_t,size_t> ;
+  auto all = std::experimental::array_property::all ;
 
   int buffer[10*20*30*40];
 
   array_t b(buffer,10);
 
+  auto a = std::experimental::subarray( b , all , all , all , all );
   auto c = std::experimental::subarray( b , 1 , 1 , 1 , 1 );
   auto d = std::experimental::subarray( b , range_t(1,2), range_t(1,3), range_t(1,4), range_t(1,5) );
+
+  // bug in gcc does not produce initializer list for template arguments
+  // auto e = std::experimental::subarray( b , {1,2} , {1,3} , {1,4} , {1,5} );
+
+  static_assert( a.rank() == 4 , "" );
+  assert( a.extent(0) == b.extent(0) );
+  assert( a.extent(1) == b.extent(1) );
+  assert( a.extent(2) == b.extent(2) );
+  assert( a.extent(3) == b.extent(3) );
+  assert( a.stride(0) == b.stride(0) );
+  assert( a.stride(1) == b.stride(1) );
+  assert( a.stride(2) == b.stride(2) );
+  assert( a.stride(3) == b.stride(3) );
+  assert( a.is_regular() == true );
+  assert( a.is_unique() == true );
+  assert( a.is_contiguous() == true );
 
   static_assert( c.rank() == 0 , "" );
   static_assert( d.rank() == 4 , "" );
@@ -119,6 +137,11 @@ void test_array_subarray()
   assert( d.extent(1) == 2 );
   assert( d.extent(2) == 3 );
   assert( d.extent(3) == 4 );
+
+  assert( d.stride(0) == b.stride(0) );
+  assert( d.stride(1) == b.stride(1) );
+  assert( d.stride(2) == b.stride(2) );
+  assert( d.stride(3) == b.stride(3) );
   assert( & b(1,1,1,1) == & d(0,0,0,0) );
   assert( & b(1,2,3,4) == & d(0,1,2,3) );
 
@@ -126,9 +149,21 @@ void test_array_subarray()
   assert( d.is_unique() == true );
   assert( d.is_contiguous() == false );
 
-  // bug in gcc does not produce initializer list for template arguments
-  //
-  // auto e = std::experimental::subarray( b , {1,2} , {1,3} , {1,4} , {1,5} );
+  // Subarray of subarray, dropping a rank
+  auto dd = std::experimental::subarray( d , 1 , range_t(1,2) , range_t(1,3) , range_t(1,4) );
+
+  assert( dd.is_regular() == true );
+  assert( dd.is_unique() == true );
+  assert( dd.is_contiguous() == false );
+  static_assert( dd.rank() == 3 , "");
+  assert( dd.extent(0) == 1 );
+  assert( dd.extent(1) == 2 );
+  assert( dd.extent(2) == 3 );
+  assert( dd.stride(0) == b.stride(1) );
+  assert( dd.stride(1) == b.stride(2) );
+  assert( dd.stride(2) == b.stride(3) );
+  assert( & d(1,1,1,1) == & dd(0,0,0) );
+  assert( & d(1,1,2,3) == & dd(0,1,2) );
 }
 
 
