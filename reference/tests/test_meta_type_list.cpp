4ae2631e65d8b18e7543c9194bf96f4fb09fb4f2
Bryce Adelstein-Lelbach aka wash <balelbach@lbl.gov>
2016-12-08 21:17:21 -0800

    Add (mostly complete) type_list tests.

---
+++ reference/tests/test_meta_type_list.cpp
@@ -0,0 +1,444 @@
+////////////////////////////////////////////////////////////////////////////////
+//  Copyright (c) 2015 Bryce Adelstein Lelbach aka wash
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+////////////////////////////////////////////////////////////////////////////////
+
+#include <boost/core/lightweight_test.hpp>
+
+#include <mdspan>
+
+using std::true_type;
+using std::false_type;
+using std::is_same;
+
+using std::integral_constant;
+using std::experimental::integer_sequence;
+using std::experimental::detail::integral_pair;
+
+using std::experimental::detail::type_list;
+using std::experimental::detail::type_list_push_front;
+using std::experimental::detail::type_list_sort;
+
+using std::experimental::detail::make_key_value_type_list_from_integer_sequence;
+
+int main()
+{
+    // type_list_push_front
+
+    {
+        BOOST_TEST_EQ((is_same<
+            type_list_push_front<int, type_list<> >::type
+          , type_list<int>
+        >::value), true);
+
+        BOOST_TEST_EQ((is_same<
+            type_list_push_front<int, type_list<int> >::type
+          , type_list<int, int>
+        >::value), true);
+
+        BOOST_TEST_EQ((is_same<
+            type_list_push_front<int, type_list<int, int> >::type
+          , type_list<int, int, int>
+        >::value), true);
+
+        BOOST_TEST_EQ((is_same<
+            type_list_push_front<int, type_list<> >::type
+          , type_list<int>
+        >::value), true);
+
+        BOOST_TEST_EQ((is_same<
+            type_list_push_front<int, type_list<char> >::type
+          , type_list<int, char>
+        >::value), true);
+
+        BOOST_TEST_EQ((is_same<
+            type_list_push_front<int, type_list<char, bool> >::type
+          , type_list<int, char, bool>
+        >::value), true);
+    }
+
+    // type_list_sort
+    
+    { // 01
+        using A = type_list<
+            integral_constant<int, 0>
+          , integral_constant<int, 1>
+        >;
+
+        using B = type_list<
+            integral_constant<int, 0>
+          , integral_constant<int, 1>
+        >;
+
+        using A_sorted = type_list_sort<A>::type;
+
+        BOOST_TEST_EQ((is_same<A, B>::value), true);
+        BOOST_TEST_EQ((is_same<A_sorted, B>::value), true);
+    }
+
+    { // 10
+        using A = type_list<
+            integral_constant<int, 1>
+          , integral_constant<int, 0>
+        >;
+
+        using B = type_list<
+            integral_constant<int, 0>
+          , integral_constant<int, 1>
+        >;
+
+        using A_sorted = type_list_sort<A>::type;
+
+        BOOST_TEST_EQ((!is_same<A, B>::value), true);
+        BOOST_TEST_EQ((is_same<A_sorted, B>::value), true);
+    }
+
+    { // 012
+        using A = type_list<
+            integral_constant<int, 0>
+          , integral_constant<int, 1>
+          , integral_constant<int, 2>
+        >;
+
+        using B = type_list<
+            integral_constant<int, 0>
+          , integral_constant<int, 1>
+          , integral_constant<int, 2>
+        >;
+
+        using A_sorted = type_list_sort<A>::type;
+
+        BOOST_TEST_EQ((is_same<A, B>::value), true);
+        BOOST_TEST_EQ((is_same<A_sorted, B>::value), true);
+    }
+
+    { // 021
+        using A = type_list<
+            integral_constant<int, 0>
+          , integral_constant<int, 2>
+          , integral_constant<int, 1>
+        >;
+
+        using B = type_list<
+            integral_constant<int, 0>
+          , integral_constant<int, 1>
+          , integral_constant<int, 2>
+        >;
+
+        using A_sorted = type_list_sort<A>::type;
+
+        BOOST_TEST_EQ((!is_same<A, B>::value), true);
+        BOOST_TEST_EQ((is_same<A_sorted, B>::value), true);
+    }
+
+    { // 102
+        using A = type_list<
+            integral_constant<int, 1>
+          , integral_constant<int, 0>
+          , integral_constant<int, 2>
+        >;
+
+        using B = type_list<
+            integral_constant<int, 0>
+          , integral_constant<int, 1>
+          , integral_constant<int, 2>
+        >;
+
+        using A_sorted = type_list_sort<A>::type;
+
+        BOOST_TEST_EQ((!is_same<A, B>::value), true);
+        BOOST_TEST_EQ((is_same<A_sorted, B>::value), true);
+    }
+
+    { // 120
+        using A = type_list<
+            integral_constant<int, 1>
+          , integral_constant<int, 2>
+          , integral_constant<int, 0>
+        >;
+
+        using B = type_list<
+            integral_constant<int, 0>
+          , integral_constant<int, 1>
+          , integral_constant<int, 2>
+        >;
+
+        using A_sorted = type_list_sort<A>::type;
+
+        BOOST_TEST_EQ((!is_same<A, B>::value), true);
+        BOOST_TEST_EQ((is_same<A_sorted, B>::value), true);
+    }
+
+    { // 201
+        using A = type_list<
+            integral_constant<int, 2>
+          , integral_constant<int, 0>
+          , integral_constant<int, 1>
+        >;
+
+        using B = type_list<
+            integral_constant<int, 0>
+          , integral_constant<int, 1>
+          , integral_constant<int, 2>
+        >;
+
+        using A_sorted = type_list_sort<A>::type;
+
+        BOOST_TEST_EQ((!is_same<A, B>::value), true);
+        BOOST_TEST_EQ((is_same<A_sorted, B>::value), true);
+    }
+
+    { // 210
+        using A = type_list<
+            integral_constant<int, 2>
+          , integral_constant<int, 1>
+          , integral_constant<int, 0>
+        >;
+
+        using B = type_list<
+            integral_constant<int, 0>
+          , integral_constant<int, 1>
+          , integral_constant<int, 2>
+        >;
+
+        using A_sorted = type_list_sort<A>::type;
+
+        BOOST_TEST_EQ((!is_same<A, B>::value), true);
+        BOOST_TEST_EQ((is_same<A_sorted, B>::value), true);
+    }
+
+    { // 139
+        using A = type_list<
+            integral_constant<int, 1>
+          , integral_constant<int, 3>
+          , integral_constant<int, 9>
+        >;
+
+        using B = type_list<
+            integral_constant<int, 1>
+          , integral_constant<int, 3>
+          , integral_constant<int, 9>
+        >;
+
+        using A_sorted = type_list_sort<A>::type;
+
+        BOOST_TEST_EQ((is_same<A, B>::value), true);
+        BOOST_TEST_EQ((is_same<A_sorted, B>::value), true);
+    }
+
+    { // 391
+        using A = type_list<
+            integral_constant<int, 3>
+          , integral_constant<int, 9>
+          , integral_constant<int, 1>
+        >;
+
+        using B = type_list<
+            integral_constant<int, 1>
+          , integral_constant<int, 3>
+          , integral_constant<int, 9>
+        >;
+
+        using A_sorted = type_list_sort<A>::type;
+
+        BOOST_TEST_EQ((!is_same<A, B>::value), true);
+        BOOST_TEST_EQ((is_same<A_sorted, B>::value), true);
+    }
+
+    {
+        using A = type_list<
+            integral_constant<int, 9>
+          , integral_constant<int, 1>
+          , integral_constant<int, 2>
+          , integral_constant<int, 3>
+          , integral_constant<int, 5>
+          , integral_constant<int, 4>
+          , integral_constant<int, 7>
+          , integral_constant<int, 8>
+          , integral_constant<int, 0>
+          , integral_constant<int, 6>
+        >;
+
+        using B = type_list<
+            integral_constant<int, 0>
+          , integral_constant<int, 1>
+          , integral_constant<int, 2>
+          , integral_constant<int, 3>
+          , integral_constant<int, 4>
+          , integral_constant<int, 5>
+          , integral_constant<int, 6>
+          , integral_constant<int, 7>
+          , integral_constant<int, 8>
+          , integral_constant<int, 9>
+        >;
+
+        using A_sorted = type_list_sort<A>::type;
+
+        BOOST_TEST_EQ((!is_same<A, B>::value), true);
+        BOOST_TEST_EQ((is_same<A_sorted, B>::value), true);
+    }
+
+    // make_key_value_type_list_from_integer_sequence
+
+    { // 01
+        using A = make_key_value_type_list_from_integer_sequence<
+            integer_sequence<int, 0, 1>
+        >::type;
+
+        using B = type_list<
+            integral_pair<std::size_t, int, 0, 0>
+          , integral_pair<std::size_t, int, 1, 1>
+        >;
+
+        BOOST_TEST_EQ((is_same<A, B>::value), true);
+    }
+
+    { // 10
+        using A = make_key_value_type_list_from_integer_sequence<
+            integer_sequence<int, 1, 0>
+        >::type;
+
+        using B = type_list<
+            integral_pair<std::size_t, int, 0, 1>
+          , integral_pair<std::size_t, int, 1, 0>
+        >;
+
+        BOOST_TEST_EQ((is_same<A, B>::value), true);
+    }
+
+    { // 012
+        using A = make_key_value_type_list_from_integer_sequence<
+            integer_sequence<int, 0, 1, 2>
+        >::type;
+
+        using B = type_list<
+            integral_pair<std::size_t, int, 0, 0>
+          , integral_pair<std::size_t, int, 1, 1>
+          , integral_pair<std::size_t, int, 2, 2>
+        >;
+
+        BOOST_TEST_EQ((is_same<A, B>::value), true);
+    }
+
+    { // 021
+        using A = make_key_value_type_list_from_integer_sequence<
+            integer_sequence<int, 0, 2, 1>
+        >::type;
+
+        using B = type_list<
+            integral_pair<std::size_t, int, 0, 0>
+          , integral_pair<std::size_t, int, 1, 2>
+          , integral_pair<std::size_t, int, 2, 1>
+        >;
+
+        BOOST_TEST_EQ((is_same<A, B>::value), true);
+    }
+
+    { // 102
+        using A = make_key_value_type_list_from_integer_sequence<
+            integer_sequence<int, 1, 0, 2>
+        >::type;
+
+        using B = type_list<
+            integral_pair<std::size_t, int, 0, 1>
+          , integral_pair<std::size_t, int, 1, 0>
+          , integral_pair<std::size_t, int, 2, 2>
+        >;
+
+        BOOST_TEST_EQ((is_same<A, B>::value), true);
+    }
+
+    { // 120
+        using A = make_key_value_type_list_from_integer_sequence<
+            integer_sequence<int, 1, 2, 0>
+        >::type;
+
+        using B = type_list<
+            integral_pair<std::size_t, int, 0, 1>
+          , integral_pair<std::size_t, int, 1, 2>
+          , integral_pair<std::size_t, int, 2, 0>
+        >;
+
+        BOOST_TEST_EQ((is_same<A, B>::value), true);
+    }
+
+    { // 201
+        using A = make_key_value_type_list_from_integer_sequence<
+            integer_sequence<int, 2, 0, 1>
+        >::type;
+
+        using B = type_list<
+            integral_pair<std::size_t, int, 0, 2>
+          , integral_pair<std::size_t, int, 1, 0>
+          , integral_pair<std::size_t, int, 2, 1>
+        >;
+
+        BOOST_TEST_EQ((is_same<A, B>::value), true);
+    }
+
+    { // 210
+        using A = make_key_value_type_list_from_integer_sequence<
+            integer_sequence<int, 2, 1, 0>
+        >::type;
+
+        using B = type_list<
+            integral_pair<std::size_t, int, 0, 2>
+          , integral_pair<std::size_t, int, 1, 1>
+          , integral_pair<std::size_t, int, 2, 0>
+        >;
+
+        BOOST_TEST_EQ((is_same<A, B>::value), true);
+    }
+
+    { // 0123
+        using A = make_key_value_type_list_from_integer_sequence<
+            integer_sequence<int, 0, 1, 2, 3>
+        >::type;
+
+        using B = type_list<
+            integral_pair<std::size_t, int, 0, 0>
+          , integral_pair<std::size_t, int, 1, 1>
+          , integral_pair<std::size_t, int, 2, 2>
+          , integral_pair<std::size_t, int, 3, 3>
+        >;
+
+        BOOST_TEST_EQ((is_same<A, B>::value), true);
+    }
+
+    { // 3210
+        using A = make_key_value_type_list_from_integer_sequence<
+            integer_sequence<int, 3, 2, 1, 0>
+        >::type;
+
+        using B = type_list<
+            integral_pair<std::size_t, int, 0, 3>
+          , integral_pair<std::size_t, int, 1, 2>
+          , integral_pair<std::size_t, int, 2, 1>
+          , integral_pair<std::size_t, int, 3, 0>
+        >;
+
+        BOOST_TEST_EQ((is_same<A, B>::value), true);
+    }
+
+    { // 1320
+        using A = make_key_value_type_list_from_integer_sequence<
+            integer_sequence<int, 1, 3, 2, 0>
+        >::type;
+
+        using B = type_list<
+            integral_pair<std::size_t, int, 0, 1>
+          , integral_pair<std::size_t, int, 1, 3>
+          , integral_pair<std::size_t, int, 2, 2>
+          , integral_pair<std::size_t, int, 3, 0>
+        >;
+
+        BOOST_TEST_EQ((is_same<A, B>::value), true);
+    }
+
+    // make_integer_sequence_inverse_mapping
+
+    return boost::report_errors();
+}
+
