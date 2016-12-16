613e39bdcb0015c3e07307fa5930b243d750a800
Bryce Adelstein-Lelbach aka wash <balelbach@lbl.gov>
2016-12-16 13:04:44 -0800

    Add sanity test for layout_regular_indexer

---
+++ reference/tests/test_layout_regular_indexer_basics.cpp
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////////////////////////
+//  Copyright (c) 2015 Bryce Adelstein Lelbach aka wash
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+////////////////////////////////////////////////////////////////////////////////
+
+#include <boost/core/lightweight_test.hpp>
+
+#include "detail/layout_regular_indexer.hpp"
+
+using std::experimental::detail::layout_regular_indexer;
+
+int main()
+{
+    // TODO
+
+    return boost::report_errors();
+}
