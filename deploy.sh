e055efb1cadaf0684dcb7f1c0b3d01ef63a865cb
David Hollman <dshollm@sandia.gov>
2018-01-17 16:09:04 -0800

    still messing with automatic generation...

--- deploy.sh
+++ deploy.sh
@@ -57,7 +57,8 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out ../deploy_key -d
+#openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out ../deploy_key -d
+openssl aes-256-cbc -K $encrypted_ebf51a80cd1a_key -iv $encrypted_ebf51a80cd1a_iv -in deploy_key.enc -out deploy_key -d
 chmod 600 ../deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
